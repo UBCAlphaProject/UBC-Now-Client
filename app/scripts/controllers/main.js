916733d2a7d22e730c9c6b6458289351ae2bce34
Tristan Rice <rice@fn.lc>
2014-11-25 21:22:58 -0800

    Added material

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,33 @@
  */
 angular.module('ubcNowClientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.items = [{
+      name: "You've got class!",
+      body: 'Woof'
+    }, {
+      name: "You've got class!",
+      body: 'Woof'
+    }];
+    var moving = [];
+    $('body').on('mousedown', '.item', function(e) {
+      console.log('woof', e);
+      moving.push([this, e.clientX]);
+      e.preventDefault();
+    }).on('mousemove', function(e) {
+      _.each(moving, function(v) {
+        var elem = v[0];
+        var x = v[1];
+        var left = e.clientX - x;
+        $(elem).css({
+          left: left,
+          opacity: Math.abs((100)/left)
+        });
+      });
+    }).on('mouseup', function(e) {
+      moving = [];
+      $('.item').animate({
+        left: 0,
+        opacity: 1
+      });
+    });
   });
